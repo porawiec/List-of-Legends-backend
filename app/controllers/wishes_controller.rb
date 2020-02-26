0d86216f4025e305835e26c5f20daf9f68fd2e4d
porawiec <54656651+porawiec@users.noreply.github.com>
2020-02-26 17:07:26 -0600

    added create functionality for wishes controller

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,2 +1,29 @@
 class WishesController < ApplicationController
+
+    def create
+        @wish = Wish.new(wish_params)
+
+        if @wish.save
+            render json: { wish: WishSerializer.new(@wish), status: created
+        else
+            render json: { error: 'failed to create wish' }
+        end
+    end
+
+    def delete
+        Wish.find(params[:id]).destroy
+    end
+
+    def wish_params
+        params.require(:wish).permit(:user_id, :skin_id)
+    end
+
 end
+
+
+@user = User.create(user_params)
+if @user.valid?
+  @token = encode_token(user_id: @user.id)
+  render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+else
+  render json: { error: 'failed to create user' }
